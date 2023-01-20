07c3b3818a59859083ea51401fa8420c45a860d4
Moritz Thüning <moritz.thuening@gmail.com>
2023-01-20 22:41:43 +0100

    Add preprocess and postprocess

--- model/model.go
+++ model/model.go
@@ -7,6 +7,9 @@ package model
 import "C"
 
 import (
+	"image"
+	"log"
+	"os"
 	"reflect"
 	"unsafe"
 )
@@ -47,5 +50,42 @@ func (model Model) Run(input []float32) (output []float32) {
 	outputSliceHeader.Data = (uintptr)(unsafe.Pointer(outputPointer))
 	outputSliceHeader.Len = model.outputLength
 	outputSliceHeader.Cap = model.outputLength
+	return
+}
+
+func Preprocess(filename string) (output []float32) {
+	reader, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	image, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bounds := image.Bounds()
+	output = make([]float32, bounds.Dx()*bounds.Dy()*3)
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := image.At(x, y).RGBA()
+			pixelIndex := (y*bounds.Max.X + x) * 3
+			output[pixelIndex] = (float32)(r) / 0xffff
+			output[pixelIndex+1] = (float32)(g) / 0xffff
+			output[pixelIndex+2] = (float32)(b) / 0xffff
+		}
+	}
+	return
+}
+
+func Postprocess(input []float32) (output float32) {
+	var maxPersonConfidence float32 = 0
+	nBoxes := 1 * 52 * 52 * 3
+	for i := 0; i < nBoxes; i++ {
+		personConfidenceIndex := i*85 + 5
+		if input[personConfidenceIndex] > maxPersonConfidence {
+			maxPersonConfidence = input[personConfidenceIndex]
+		}
+	}
+	output = maxPersonConfidence
 	return
 }
